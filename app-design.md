22e47af5a451a94eafdd65ca2bce1e76432ea4fc
Amy Lyon <amy_lyon@progressive.com>
2022-12-12 11:40:00 -0500

    api setup

---
+++ app-design.md
@@ -0,0 +1,7 @@
+# Our Application
+
+## Our API
+
+### HTTP-Based
+
+
---
+++ backend/GiftingSolution/GiftingAPI/Controllers/Models/People.cs
@@ -0,0 +1,5 @@
+﻿namespace GiftingApi.Models;
+
+public record PersonItemResponse(string Id, string FirstName, string LastName);
+
+public record PersonResponse(List<PersonItemResponse> Data);
