521a4686814c92bb591f55e8dc614375bd60c83e
Amy Lyon <amy_lyon@progressive.com>
2022-12-14 15:39:38 -0500

    prefs

---
+++ frontend/gifting-frontend/src/app/features/counter/state/index.ts
@@ -0,0 +1,19 @@
+import { ActionReducerMap, createFeatureSelector, createSelector } from "@ngrx/store";
+import * as fromCount from './reducers/count-reducer';
+
+export const featureName = "counter";
+
+export interface CounterState {
+    count: fromCount.CountState
+}
+
+export const reducers: ActionReducerMap<CounterState> = {
+    count: fromCount.reducer
+};
+
+const selectFeature = createFeatureSelector<CounterState>(featureName);
+
+const selectCountBranch = createSelector(selectFeature, f => f.count);
+
+export const selectCountCurrent = createSelector(selectCountBranch, b => b.current);
+export const selectCountingBy = createSelector(selectCountBranch, b => b.by);
